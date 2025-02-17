4ae3a04c733ea5ee51350539984265c123ccedfe
Deimos <Cneallego@gmail.com>
2025-02-16 22:40:00 -0500

    member set up

--- mvp-v1/olympiad_prep_app/members/urls.py
+++ mvp-v1/olympiad_prep_app/members/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('members/', views.index, name="index"),
+    path('members/', views.myuser_dashboard, name='myuser_dashboard'),
+    path('members/<str:memberName>/', views.user_dashboard, name='user_dashboard')
     #path("login/", views.login_view, name="login"),
-    #path("logout/", views.logout_view, name="logout")
+    #path("logout/", views.logout_view, name="logout"),
+    #path('dashboard/', views.user_dashboard, name='user_dashboard'),  # Example URL
+    #path('document/<int:document_id>/', views.document_detail, name='document_detail'), # Example document detail URL
+    #path('document/create/', views.document_create, name='document_create'), # Example document create URL
+    #path('mindmap/<int:mindmap_id>/', views.mindmap_detail, name='mindmap_detail'), # Example mindmap detail URL
+    #path('mindmap/create/', views.mindmap_create, name='mindmap_create'), # Example mindmap create URL
 ]
--- mvp-v1/olympiad_prep_app/members/views.py
+++ mvp-v1/olympiad_prep_app/members/views.py
@@ -1,11 +1,56 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 import json
 from django.contrib.auth import authenticate, login, logout
 
+from django.contrib.auth.decorators import login_required  # Import the login_required decorator
+from members.models import Document, Mindmap, Member
 
 # Create your views here.
 
 def index(request):
     return render(request, "home.html")
+
+
+
+@login_required  # Protects the view; redirects to login if not logged in
+def myuser_dashboard(request):
+    if Member.objects.filter(authuser=request.user).exists():  
+        member = Member.objects.filter(authuser = request.user)[0]
+        context = {
+        'documents': Document.objects.filter(creatorId = member.userId),
+        'mindmaps': Mindmap.objects.filter(creatorId = member.userId),
+        'username':member.username,
+        'user':request.user,
+        'member':member,
+        'friends': Member.objects.filter(friends = member),
+        }
+        return render(request, 'members/myuser_dashboard.html', context)
+    else:
+        member = Member.objects.create(
+        username=request.user.username,
+        authuser= request.user,
+        )
+        context = {
+            'username':member.username,
+            }
+        #return render(request, 'user_dashboard.html', context)
+    return HttpResponse("No Response")
+
+
+@login_required  # Protects the view; redirects to login if not logged in
+def user_dashboard(request, memberName):
+    if Member.objects.filter(username = memberName).exists():  
+        member = Member.objects.filter(username = memberName)[0]
+        context = {
+        'documents': Document.objects.filter(creatorId = member.userId),
+        'mindmaps': Mindmap.objects.filter(creatorId = member.userId),
+        'username':member.username,
+        'user':request.user,
+        'member':member,
+        'friends': Member.objects.filter(friends = member),
+        }
+        return render(request, 'members/user_dashboard.html', context)
+    return HttpResponse("Not an User")
+  
